4117460ea96bcec60e5f8aa85e726783783bf522
Philip Watson <philip.watson@adscale.co.nz>
2019-09-19 12:31:04 +1200

    Make fake dfp provide substitution macros like the real implementation

--- extensions/amp-ad-network-fakedfp-impl/0.1/amp-ad-network-fakedfp-impl.js
+++ extensions/amp-ad-network-fakedfp-impl/0.1/amp-ad-network-fakedfp-impl.js
@@ -2,6 +2,10 @@ import '../../amp-a4a/0.1/real-time-config-manager';
 import {AmpA4A} from '../../amp-a4a/0.1/amp-a4a';
 import {Deferred} from '../../../src/utils/promise';
 import {buildUrl} from './../../../ads/google/a4a/shared/url-builder';
+import {Services} from '../../../src/services';
+import {tryParseJson} from '../../../src/json';
+import {getOrCreateAdCid} from '../../../src/ad-cid';
+import {dev} from '../../../src/log';
 
 /** @const {string} */
 const FAKEDFP_BASE_URL = 'https://prebid-support.lsd.test/ads';
@@ -75,13 +79,57 @@ export class AmpAdNetworkFakedfpImpl extends AmpA4A {
     return size;
   }
 
+  /** @override */
+  getCustomRealTimeConfigMacros_() {
+    /**
+     * This whitelist allow attributes on the amp-ad element to be used as
+     * macros for constructing the RTC URL. Add attributes here, in lowercase,
+     * to make them available.
+     */
+    const whitelist = {
+      'height': true,
+      'width': true,
+      'data-slot': true,
+      'data-multi-size': true,
+      'data-multi-size-validation': true,
+      'data-override-width': true,
+      'data-override-height': true,
+    };
+
+    return {
+      PAGEVIEWID: () => Services.documentInfoForDoc(this.element).pageViewId,
+      HREF: () => this.win.location.href,
+      REFERRER: opt_timeout => this.getReferrer_(opt_timeout),
+      TGT: () =>
+        JSON.stringify(
+          (tryParseJson(this.element.getAttribute('json')) || {})['targeting']
+        ),
+      ADCID: opt_timeout =>
+        getOrCreateAdCid(
+          this.getAmpDoc(),
+          'AMP_ECID_GOOGLE',
+          '_ga',
+          parseInt(opt_timeout, 10)
+        ),
+      ATTR: name => {
+        if (!whitelist[name.toLowerCase()]) {
+          dev().warn('TAG', `Invalid attribute ${name}`);
+        } else {
+          return this.element.getAttribute(name);
+        }
+      },
+      CANONICAL_URL: () =>
+        Services.documentInfoForDoc(this.element).canonicalUrl,
+    };
+  }
+
   handleResize_(newWidth, newHeight) {
     const {width, height} = this.getDeclaredSlotSize_();
     const returnedSizeDifferent = newWidth !== width || newHeight !== height;
     if (returnedSizeDifferent) {
       console.log(`Attempt to change size from ${width}x${height} to ${newWidth}x${newHeight}...`);
       this.attemptChangeSize(newHeight, newWidth)
-        .then( () => { console.log('Change size successful.'); })
+        .then(() => { console.log('Change size successful.'); })
         .catch(() => { console.log('Change size denied.'); });
     }
   }
